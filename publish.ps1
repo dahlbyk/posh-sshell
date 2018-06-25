9cdd816282c368b63cf164b570fc2ec3d30d59b2
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2018-06-25 23:16:27 +0100

    Add prompt to publish script

--- publish.ps1
+++ publish.ps1
@@ -8,8 +8,9 @@ param(
 )
 
 if (!$NugetApiKey) {
+    $homePath = if ($Env:HOME) {$Env:HOME} else {$Home}
     # Allow api key to be explicitly specified, or find it locally.
-    $keyfile = "$Env:USERPROFILE\Dropbox\powershellgallery-access-key.txt"
+    $keyfile = "$homePath\Dropbox\powershellgallery-access-key.txt"
 
     if(!(Test-Path $keyfile)) {
         throw "No NuGet access key specified."
@@ -24,12 +25,24 @@ Remove-Item "$PSScriptRoot\.build" -Force -Recurse -ErrorAction Ignore
 New-Item -Type Directory "$PSScriptRoot\.build" -ErrorAction Ignore | Out-Null
 
 # Copy everything we want into .build
-Copy-Item "$PSScriptRoot\posh-sshell.psd1" "$PSScriptRoot\.build"
-Copy-Item "$PSScriptRoot\posh-sshell.psm1" "$PSScriptRoot\.build"
-Copy-Item "$PSScriptRoot\LICENSE.txt" "$PSScriptRoot\.build"
-Copy-Item "$PSScriptRoot\src\" "$PSScriptRoot\.build" -Recurse
+Copy-Item "posh-sshell.psd1" ".build"
+Copy-Item "posh-sshell.psm1" ".build"
+Copy-Item "LICENSE.txt" ".build"
+Copy-Item "src\" ".build" -Recurse
 
-# Run publish-module against the build dir
-Publish-Module -Name "$PSScriptRoot\.build\posh-sshell.psd1" -NuGetApiKey $NugetApiKey
+$options = @(
+    [System.Management.Automation.Host.ChoiceDescription]::new('&No', 'No')
+    [System.Management.Automation.Host.ChoiceDescription]::new('&Yes', 'Yes')
+)
 
-Write-Host "Done."
+$module = Invoke-Expression (Get-Content ".\posh-sshell.psd1" -Raw)
+$version = $module.ModuleVersion
+
+$confirm = $host.ui.PromptForChoice("Publish release $version to PS Gallery?", "", $options, 0)
+
+if($confirm) {
+    Write-Host "Publishing..."
+    # Run publish-module against the build dir
+    Publish-Module -Name "$PSScriptRoot\.build\posh-sshell.psd1" -NuGetApiKey $NugetApiKey
+    Write-Host "Done."
+}
